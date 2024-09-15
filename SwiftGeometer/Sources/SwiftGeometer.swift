d322baf6fb1514b62a590ccea80110de3b1e9161
Alex Hunsley <alex.hunsley@gmail.com>
2024-09-15 14:00:42 +0100

    Add some constants and tests

--- SwiftGeometer/Sources/SwiftGeometer.swift
+++ SwiftGeometer/Sources/SwiftGeometer.swift
@@ -5,3 +5,12 @@ public struct SwiftGeometer {
         "Hello from SwiftGeometer"
     }
 }
+
+// Pi for Double, Float, CGFloat, etc.
+extension BinaryFloatingPoint {
+    public static var pi: Self { Self(Double.pi) }
+    public static var pi2: Self { pi / 2 }
+    public static var pi4: Self { pi / 4 }
+    public static var pi8: Self { pi / 8 }
+    public static var tau: Self { pi * 2 }
+}
--- SwiftGeometer/Tests/SwiftGeometerTests.swift
+++ SwiftGeometer/Tests/SwiftGeometerTests.swift
@@ -1,8 +1,32 @@
 import Foundation
 import XCTest
+import SwiftGeometer
 
 final class SwiftGeometerTests: XCTestCase {
-    func test_twoPlusTwo_isFour() {
-        XCTAssertEqual(2+2, 4)
+
+//    func assertEqualEpsilon<T: FloatingPoint>(a: T, b: T) {
+//    func assertEqualEpsilon(a: FloatingPoint, b: FloatingPoint) {
+//        XCTAssertEqual(a, b, accuracy: T(0.00001))
+//    }
+
+    func test_whenAccessingConstants_thenCorrectValuesFound() {
+        XCTAssertEqual(Double.pi, 3.1415926, accuracy: 0.00001)
+        XCTAssertEqual(Double.pi2, 1.5707963, accuracy: 0.00001)
+        XCTAssertEqual(Double.pi4, 0.78539815, accuracy: 0.00001)
+        XCTAssertEqual(Double.pi8, 0.392699075, accuracy: 0.00001)
+        XCTAssertEqual(Double.tau, 6.2831852, accuracy: 0.00001)
+
+        XCTAssertEqual(Float.pi, 3.1415926, accuracy: 0.00001)
+        XCTAssertEqual(Float.pi2, 1.5707963, accuracy: 0.00001)
+        XCTAssertEqual(Float.pi4, 0.78539815, accuracy: 0.00001)
+        XCTAssertEqual(Float.pi8, 0.392699075, accuracy: 0.00001)
+        XCTAssertEqual(Float.tau, 6.2831852, accuracy: 0.00001)
+
+        XCTAssertEqual(CGFloat.pi, 3.1415926, accuracy: 0.00001)
+        XCTAssertEqual(CGFloat.pi2, 1.5707963, accuracy: 0.00001)
+        XCTAssertEqual(CGFloat.pi4, 0.78539815, accuracy: 0.00001)
+        XCTAssertEqual(CGFloat.pi8, 0.392699075, accuracy: 0.00001)
+        XCTAssertEqual(CGFloat.tau, 6.2831852, accuracy: 0.00001)
+
     }
 }
