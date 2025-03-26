0d36a003b95274bdaefd58f135e285ef4b7c99f3
Alex Hunsley <alex.hunsley@gmail.com>
2025-03-26 15:47:10 +0000

    Add to tests for kite/concave

--- SwiftGeometer/Tests/PolygonTests.swift
+++ SwiftGeometer/Tests/PolygonTests.swift
@@ -31,8 +31,8 @@ struct PolygonTests {
 
         // to left of kite bit, inside. fails with convex only alg!
         (Vec2(x: 1.0, y: 7.5), true),
-//        (Vec2(x: 0.1, y: 7.5), true),
-//        (Vec2(x: 0.1, y: 2.5), true),
+        (Vec2(x: 0.1, y: 7.5), true),
+        (Vec2(x: 0.1, y: 2.5), true),
         (Vec2(x: 9.0, y: 7.5), true),
 
         (Vec2(x: 4.01, y: 5.99), true),
@@ -41,6 +41,11 @@ struct PolygonTests {
         (Vec2(x: 5.0, y: 5), true),
         (Vec2(x: 5.0, y: 1), true),
         (Vec2(x: 5.0, y: 0.01), true),
+
+        // outside
+        (Vec2(x: 5.0, y: 6.02), false),
+        (Vec2(x: 4.01, y: 6.02), false),
+        (Vec2(x: 5.99, y: 6.02), false),
 
 
         // to right of kite bit, inside. fails with convex only alg!
