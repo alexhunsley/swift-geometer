a57bac345f3f4a22ea8788af6ba5b3e8131fa1ff
Alex Hunsley <alex.hunsley@gmail.com>
2025-03-26 18:44:08 +0000

    Tidy

--- SwiftGeometer/Sources/PolygonHit.swift
+++ SwiftGeometer/Sources/PolygonHit.swift
@@ -48,24 +48,16 @@ public func polygon(vertices: [Vec2], containsPoint point: Vec2) -> Bool {
         print("**  edge = \(edge), curr edge is right turn = \(edgeIsRightTurn)")
 
         if edgeIsRightTurn {
+            print("**  >>> right turn, reset numLefts to 0")
             numLefts = 0
-            print("**  >>> right turn")
-//            isInsideCurrentCavity = false
-
             if failOnNextRight {
-//            if isInsideCurrentCavity {
                 print("**   RET false! is right turn, and failOnNextRight == true")
-//                print("**   RET false! is right turn and isInsideCurrentCavity == true")
-
                 return false
             }
         }
         else {
             numLefts += 1
             print("**  <<< left turn (count: \(numLefts))")
-            // a left turn = default to being in a cavity.
-            // we set false if we to right at any point
-//            isInsideCurrentCavity = true
 
             if numLefts == 1, !isFirstLoop {
                 // on first left, use previous in/out
@@ -75,33 +67,21 @@ public func polygon(vertices: [Vec2], containsPoint point: Vec2) -> Bool {
 
                 failOnNextRight = prev_pointIsToLeft
                 print("**  <> assigning prev_pointIsToLeft = \(failOnNextRight) to failOnNextRight")
-//                failOnNextRight = true
-            }
-            else {
-//                failOnNextRight = true
             }
         }
-
-        // ah. add check of previous turn here.
-        //
 
         let pointIsToLeft = (point - edge.start).isToLeft(ofVector: edge.direction)
         print("**  pointIsToLeft = \(pointIsToLeft)")
 
         // keeping isFirstLoop below makes kite tests pass but not square,
         // getting rid of it does v.v.
-        if pointIsToLeft && edgeIsRightTurn { //}&& isFirstLoop {
+        if pointIsToLeft && edgeIsRightTurn {
             failOnNextRight = true
         }
         
-        if !pointIsToLeft && !edgeIsRightTurn { //}&& isFirstLoop  {
-
-//            failOnNextRight = false
-//            if !edgeIsRightTurn {
-                //            isInsideCurrentCavity = false
+        if !pointIsToLeft && !edgeIsRightTurn {
             print("**   !edgeIsRightTurn == false so setting failOnNextRight = false")
             failOnNextRight = false
-//            }
         }
         prev_pointIsToLeft = pointIsToLeft
 
