30d70ed963b68bfa29de21d172feb88337ff6c51
Alex Hunsley <alex.hunsley@gmail.com>
2025-03-28 10:29:59 +0000

    Output all rots of the test ASCII output

--- SwiftGeometer/Tests/PolygonTests.swift
+++ SwiftGeometer/Tests/PolygonTests.swift
@@ -108,10 +108,13 @@ struct PolygonTests {
 //        let poly = kiteLevelTwoCavity // fails! as expected (the little bump up doesn't appear)
         let poly = levelOneCavities
 
-        test_bitmap(poly: poly, res: 1.0)
-//        print(">>>>>>>>>>>>>> verts after rot: \(poly.rotateLeft(rotationAmount))")
-//        #expect(polygon(vertices: poly.rotateLeft(rotationAmount),
-//                        containsPoint: pointsToExpected.0) == pointsToExpected.1)
+        for rots in 0...poly.count {
+            print("--------- ROTS = \(rots)")
+            test_bitmap(poly: poly.rotateLeft(rots), res: 1.0)
+            //        print(">>>>>>>>>>>>>> verts after rot: \(poly.rotateLeft(rotationAmount))")
+        }
+        #expect(polygon(vertices: poly.rotateLeft(rotationAmount),
+                    containsPoint: pointsToExpected.0) == pointsToExpected.1)
     }
 
     func boundingRect(for points: [Vec2], border: CGFloat = 0.0) -> CGRect {
@@ -170,108 +173,7 @@ struct PolygonTests {
         }
     }
 
-    @Test("kiteContainsPoint - all rots", arguments: [
-        // on boundary is inside
-        (Vec2.zero, true),
-        // in kite part, outside
-        (Vec2(x: 5.0, y: 7.5), false),
-
-        // to left of kite bit, inside. fails with convex only alg!
-        (Vec2(x: 1.0, y: 7.5), true),
-
-        (Vec2(x: 0.1, y: 7.5), true),
-        (Vec2(x: 0.1, y: 2.5), true),
-        (Vec2(x: 9.0, y: 7.5), true),
-
-        (Vec2(x: 4.01, y: 5.99), true),
-        (Vec2(x: 5.99, y: 5.99), true),
-        (Vec2(x: 5.0, y: 5.99), true),
-        (Vec2(x: 5.0, y: 5), true),
-        (Vec2(x: 5.0, y: 1), true),
-        (Vec2(x: 5.0, y: 0.01), true),
-
-        // outside
-        (Vec2(x: 5.0, y: 6.02), false),
-        (Vec2(x: 4.01, y: 6.02), false),
-        (Vec2(x: 5.99, y: 6.02), false),
-
-        // outside
-        (Vec2(x: -0.1, y: 0.1), false),
-        (Vec2(x: -0.01, y: -0.1), false),
-        (Vec2(x: 0.01, y: -0.1), false),
-        (Vec2(x: -0.01, y: 0.1), false),
-        (Vec2(x: 2.501, y: 10.01), false),
-
-        // very large offsets
-        (Vec2(x: .greatestFiniteMagnitude), false),
-        (Vec2(x: -.greatestFiniteMagnitude), false),
-        (Vec2(y: .greatestFiniteMagnitude), false),
-        (Vec2(y: -.greatestFiniteMagnitude), false),
-    ], 0...5)
-    func test_kiteContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
-        print("Rotated: \(kite.rotateLeft(rotationAmount))")
-        #expect(polygon(vertices: kite.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
-    }
-
-    @Test("kiteTwoCavityContainsPoint - all rots", arguments: [
-        // on boundary is inside
-        (Vec2.zero, true),
-        // in kite part, outside
-        (Vec2(x: 5.0, y: 7.5), false),
-
-        // to left of kite bit, inside. fails with convex only alg!
-        (Vec2(x: 1.0, y: 7.5), true),
-
-        (Vec2(x: 0.1, y: 7.5), true),
-        (Vec2(x: 0.1, y: 2.5), true),
-        (Vec2(x: 9.0, y: 7.5), true),
-
-        (Vec2(x: 4.01, y: 5.99), true),
-        (Vec2(x: 5.99, y: 5.99), true),
-        (Vec2(x: 5.0, y: 5.99), true),
-        (Vec2(x: 5.0, y: 5), true),
-        (Vec2(x: 5.0, y: 1), true),
-        (Vec2(x: 5.0, y: 1.1), true),
-        // outside points in bottom notch
-        (Vec2(x: 4.9, y: 0.01), false),
-        (Vec2(x: 5.0, y: 0.01), false),
-        (Vec2(x: 5.1, y: 0.01), false),
-
-        // outside
-        (Vec2(x: 5.0, y: 6.02), false),
-        (Vec2(x: 4.01, y: 6.02), false),
-        (Vec2(x: 5.99, y: 6.02), false),
-
-        // outside
-        (Vec2(x: -0.1, y: 0.1), false),
-        (Vec2(x: -0.01, y: -0.1), false),
-        (Vec2(x: 0.01, y: -0.1), false),
-        (Vec2(x: -0.01, y: 0.1), false),
-        (Vec2(x: 2.501, y: 10.01), false),
-
-        // very large offsets
-        (Vec2(x: .greatestFiniteMagnitude), false),
-        (Vec2(x: -.greatestFiniteMagnitude), false),
-        (Vec2(y: .greatestFiniteMagnitude), false),
-        (Vec2(y: -.greatestFiniteMagnitude), false),
-    ], 0...5)
-    func test_kiteTwoCavityContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
-        #expect(polygon(vertices: kiteTwoCavity.rotateLeft(rotationAmount),
-                        containsPoint: pointsToExpected.0) == pointsToExpected.1)
-    }
-
-//    // we expect this to fail - the alg doesn't handle anything beyond first level cavities (i.e. convex cavities).
-//    // this shape has a concave shape subtracted
-//    @Test("kiteLevelTwoCavityContainsPoint - all rots", arguments: [
-//        // THESE THREE FAIL (alg doesn't handle concave cutaways, some
-//        // failure is expected)
-//        // inside: the little inner bump
-//        (Vec2(x: 5.0, y: 6.02), true),
-//        (Vec2(x: 4.98, y: 6.02), true),
-//        (Vec2(x: 5.02, y: 6.02), true),
-//
-//        // The remaing pass:
-//
+//    @Test("kiteContainsPoint - all rots", arguments: [
 //        // on boundary is inside
 //        (Vec2.zero, true),
 //        // in kite part, outside
@@ -281,7 +183,7 @@ struct PolygonTests {
 //        (Vec2(x: 1.0, y: 7.5), true),
 //
 //        (Vec2(x: 0.1, y: 7.5), true),
-//        (Vec2(x: 0.1, y: 2.5), true), 
+//        (Vec2(x: 0.1, y: 2.5), true),
 //        (Vec2(x: 9.0, y: 7.5), true),
 //
 //        (Vec2(x: 4.01, y: 5.99), true),
@@ -291,12 +193,10 @@ struct PolygonTests {
 //        (Vec2(x: 5.0, y: 1), true),
 //        (Vec2(x: 5.0, y: 0.01), true),
 //
-//        // outside: above the little inner bump
-//        (Vec2(x: 5.0, y: 7.1), false),
-//        (Vec2(x: 4.01, y: 7.1), false),
-//        (Vec2(x: 5.99, y: 7.1), false),
-//        (Vec2(x: 4.01, y: 9.9), false),
-//        (Vec2(x: 5.99, y: 9.9), false),
+//        // outside
+//        (Vec2(x: 5.0, y: 6.02), false),
+//        (Vec2(x: 4.01, y: 6.02), false),
+//        (Vec2(x: 5.99, y: 6.02), false),
 //
 //        // outside
 //        (Vec2(x: -0.1, y: 0.1), false),
@@ -311,62 +211,165 @@ struct PolygonTests {
 //        (Vec2(y: .greatestFiniteMagnitude), false),
 //        (Vec2(y: -.greatestFiniteMagnitude), false),
 //    ], 0...5)
-//    func test_kiteLevelTwoCavityContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
-//        #expect(polygon(vertices: kiteLevelTwoCavity.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
+//    func test_kiteContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
+//        print("Rotated: \(kite.rotateLeft(rotationAmount))")
+//        #expect(polygon(vertices: kite.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
 //    }
-
-    @Test("triangleContainsPoint", arguments: [
-        // on boundary is inside
-        (Vec2.zero, true),
-        (Vec2(x: 5, y: 0), true),
-        (Vec2(x: 0, y: 20), true),
-        // centre inside
-        (Vec2(x: 2.5, y: 10), true),
-        // inside
-        (Vec2(x: 2.499, y: 9.99), true),
-        // outside
-        (Vec2(x: -0.01, y: -0.1), false),
-        (Vec2(x: 0.01, y: -0.1), false),
-        (Vec2(x: -0.01, y: 0.1), false),
-        (Vec2(x: 2.501, y: 10.01), false),
-        // very large offsets
-        (Vec2(x: .greatestFiniteMagnitude), false),
-        (Vec2(x: -.greatestFiniteMagnitude), false),
-        (Vec2(y: .greatestFiniteMagnitude), false),
-        (Vec2(y: -.greatestFiniteMagnitude), false),
-    ], 0...2)
-    func test_triangleContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
-        #expect(polygon(vertices: triangle.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
-    }
-
-    @Test("squareContainsPoint", arguments: [
-        // on boundary is inside
-        (Vec2.zero, true),
-        (Vec2(x: 10, y: 10), true),
-        // dead centre inside
-        (Vec2(x: 5.0, y: 5.0), true),
-        // 4 points near square corners (inside)
-        (Vec2(x: 0.1, y: 0.1), true),
-        (Vec2(x: 9.9, y: 0.1), true),
-        (Vec2(x: 9.9, y: 9.9), true),
-        (Vec2(x: 0.1, y: 9.9), true),
-        // 4 points near square corners (outside)
-        (Vec2(x: -0.1, y: 0.1), false),
-        (Vec2(x: 10.1, y: 0.1), false),
-        (Vec2(x: 10.1, y: 9.9), false),
-        (Vec2(x: -0.1, y: 9.9), false),
-        // 4 points near square corners (outside) B
-        (Vec2(x: 0.1, y: -0.1), false),
-        (Vec2(x: 0.9, y: -0.1), false),
-        (Vec2(x: 0.9, y: 10.1), false),
-        (Vec2(x: 0.1, y: 10.1), false),
-        // very large offsets
-        (Vec2(x: .greatestFiniteMagnitude), false),
-        (Vec2(x: -.greatestFiniteMagnitude), false),
-        (Vec2(y: .greatestFiniteMagnitude), false),
-        (Vec2(y: -.greatestFiniteMagnitude), false),
-    ], 0...3)
-    func test_squareContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
-        #expect(polygon(vertices: square.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
-    }
+//
+//    @Test("kiteTwoCavityContainsPoint - all rots", arguments: [
+//        // on boundary is inside
+//        (Vec2.zero, true),
+//        // in kite part, outside
+//        (Vec2(x: 5.0, y: 7.5), false),
+//
+//        // to left of kite bit, inside. fails with convex only alg!
+//        (Vec2(x: 1.0, y: 7.5), true),
+//
+//        (Vec2(x: 0.1, y: 7.5), true),
+//        (Vec2(x: 0.1, y: 2.5), true),
+//        (Vec2(x: 9.0, y: 7.5), true),
+//
+//        (Vec2(x: 4.01, y: 5.99), true),
+//        (Vec2(x: 5.99, y: 5.99), true),
+//        (Vec2(x: 5.0, y: 5.99), true),
+//        (Vec2(x: 5.0, y: 5), true),
+//        (Vec2(x: 5.0, y: 1), true),
+//        (Vec2(x: 5.0, y: 1.1), true),
+//        // outside points in bottom notch
+//        (Vec2(x: 4.9, y: 0.01), false),
+//        (Vec2(x: 5.0, y: 0.01), false),
+//        (Vec2(x: 5.1, y: 0.01), false),
+//
+//        // outside
+//        (Vec2(x: 5.0, y: 6.02), false),
+//        (Vec2(x: 4.01, y: 6.02), false),
+//        (Vec2(x: 5.99, y: 6.02), false),
+//
+//        // outside
+//        (Vec2(x: -0.1, y: 0.1), false),
+//        (Vec2(x: -0.01, y: -0.1), false),
+//        (Vec2(x: 0.01, y: -0.1), false),
+//        (Vec2(x: -0.01, y: 0.1), false),
+//        (Vec2(x: 2.501, y: 10.01), false),
+//
+//        // very large offsets
+//        (Vec2(x: .greatestFiniteMagnitude), false),
+//        (Vec2(x: -.greatestFiniteMagnitude), false),
+//        (Vec2(y: .greatestFiniteMagnitude), false),
+//        (Vec2(y: -.greatestFiniteMagnitude), false),
+//    ], 0...5)
+//    func test_kiteTwoCavityContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
+//        #expect(polygon(vertices: kiteTwoCavity.rotateLeft(rotationAmount),
+//                        containsPoint: pointsToExpected.0) == pointsToExpected.1)
+//    }
+//
+////    // we expect this to fail - the alg doesn't handle anything beyond first level cavities (i.e. convex cavities).
+////    // this shape has a concave shape subtracted
+////    @Test("kiteLevelTwoCavityContainsPoint - all rots", arguments: [
+////        // THESE THREE FAIL (alg doesn't handle concave cutaways, some
+////        // failure is expected)
+////        // inside: the little inner bump
+////        (Vec2(x: 5.0, y: 6.02), true),
+////        (Vec2(x: 4.98, y: 6.02), true),
+////        (Vec2(x: 5.02, y: 6.02), true),
+////
+////        // The remaing pass:
+////
+////        // on boundary is inside
+////        (Vec2.zero, true),
+////        // in kite part, outside
+////        (Vec2(x: 5.0, y: 7.5), false),
+////
+////        // to left of kite bit, inside. fails with convex only alg!
+////        (Vec2(x: 1.0, y: 7.5), true),
+////
+////        (Vec2(x: 0.1, y: 7.5), true),
+////        (Vec2(x: 0.1, y: 2.5), true), 
+////        (Vec2(x: 9.0, y: 7.5), true),
+////
+////        (Vec2(x: 4.01, y: 5.99), true),
+////        (Vec2(x: 5.99, y: 5.99), true),
+////        (Vec2(x: 5.0, y: 5.99), true),
+////        (Vec2(x: 5.0, y: 5), true),
+////        (Vec2(x: 5.0, y: 1), true),
+////        (Vec2(x: 5.0, y: 0.01), true),
+////
+////        // outside: above the little inner bump
+////        (Vec2(x: 5.0, y: 7.1), false),
+////        (Vec2(x: 4.01, y: 7.1), false),
+////        (Vec2(x: 5.99, y: 7.1), false),
+////        (Vec2(x: 4.01, y: 9.9), false),
+////        (Vec2(x: 5.99, y: 9.9), false),
+////
+////        // outside
+////        (Vec2(x: -0.1, y: 0.1), false),
+////        (Vec2(x: -0.01, y: -0.1), false),
+////        (Vec2(x: 0.01, y: -0.1), false),
+////        (Vec2(x: -0.01, y: 0.1), false),
+////        (Vec2(x: 2.501, y: 10.01), false),
+////
+////        // very large offsets
+////        (Vec2(x: .greatestFiniteMagnitude), false),
+////        (Vec2(x: -.greatestFiniteMagnitude), false),
+////        (Vec2(y: .greatestFiniteMagnitude), false),
+////        (Vec2(y: -.greatestFiniteMagnitude), false),
+////    ], 0...5)
+////    func test_kiteLevelTwoCavityContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
+////        #expect(polygon(vertices: kiteLevelTwoCavity.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
+////    }
+//
+//    @Test("triangleContainsPoint", arguments: [
+//        // on boundary is inside
+//        (Vec2.zero, true),
+//        (Vec2(x: 5, y: 0), true),
+//        (Vec2(x: 0, y: 20), true),
+//        // centre inside
+//        (Vec2(x: 2.5, y: 10), true),
+//        // inside
+//        (Vec2(x: 2.499, y: 9.99), true),
+//        // outside
+//        (Vec2(x: -0.01, y: -0.1), false),
+//        (Vec2(x: 0.01, y: -0.1), false),
+//        (Vec2(x: -0.01, y: 0.1), false),
+//        (Vec2(x: 2.501, y: 10.01), false),
+//        // very large offsets
+//        (Vec2(x: .greatestFiniteMagnitude), false),
+//        (Vec2(x: -.greatestFiniteMagnitude), false),
+//        (Vec2(y: .greatestFiniteMagnitude), false),
+//        (Vec2(y: -.greatestFiniteMagnitude), false),
+//    ], 0...2)
+//    func test_triangleContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
+//        #expect(polygon(vertices: triangle.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
+//    }
+//
+//    @Test("squareContainsPoint", arguments: [
+//        // on boundary is inside
+//        (Vec2.zero, true),
+//        (Vec2(x: 10, y: 10), true),
+//        // dead centre inside
+//        (Vec2(x: 5.0, y: 5.0), true),
+//        // 4 points near square corners (inside)
+//        (Vec2(x: 0.1, y: 0.1), true),
+//        (Vec2(x: 9.9, y: 0.1), true),
+//        (Vec2(x: 9.9, y: 9.9), true),
+//        (Vec2(x: 0.1, y: 9.9), true),
+//        // 4 points near square corners (outside)
+//        (Vec2(x: -0.1, y: 0.1), false),
+//        (Vec2(x: 10.1, y: 0.1), false),
+//        (Vec2(x: 10.1, y: 9.9), false),
+//        (Vec2(x: -0.1, y: 9.9), false),
+//        // 4 points near square corners (outside) B
+//        (Vec2(x: 0.1, y: -0.1), false),
+//        (Vec2(x: 0.9, y: -0.1), false),
+//        (Vec2(x: 0.9, y: 10.1), false),
+//        (Vec2(x: 0.1, y: 10.1), false),
+//        // very large offsets
+//        (Vec2(x: .greatestFiniteMagnitude), false),
+//        (Vec2(x: -.greatestFiniteMagnitude), false),
+//        (Vec2(y: .greatestFiniteMagnitude), false),
+//        (Vec2(y: -.greatestFiniteMagnitude), false),
+//    ], 0...3)
+//    func test_squareContainsPointRot(pointsToExpected: (Vec2, Bool), rotationAmount: Int) {
+//        #expect(polygon(vertices: square.rotateLeft(rotationAmount), containsPoint: pointsToExpected.0) == pointsToExpected.1)
+//    }
 }
